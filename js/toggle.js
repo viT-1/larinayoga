a1d3e92695378b4c39ceab8ded1892a85d2761ed
Vitaly Pinchuk <pinchuk.vit.1@gmail.com>
2017-03-03 18:55:49 +0300

    Поменял названия параметров на более верные https://github.com/viT-1/larinayoga/issues/19

--- js/toggle.js
+++ js/toggle.js
@@ -1,25 +1,25 @@
-function initTogglingCtl ( toggling_ctl, toggler_attr_name, hider_attr_name, c_less, c_more ) {
+function initTogglingCtl ( toggling_ctl, toggler_attr_name, hider_attr_name, c_init, c_click ) {
 	//'class' is backward compatibility for IE8
 	var attr_name_t = document.addEventListener ? toggler_attr_name : 'class';
 	var attr_name_h = document.addEventListener ? hider_attr_name : 'class';
 	var hider_ctl = toggling_ctl.parentNode.querySelectorAll( '[' + hider_attr_name + ']' )[0];
 	
-	addAttrVal( toggling_ctl, attr_name_t, c_less );
-	addAttrVal( hider_ctl, attr_name_h, c_less );
+	addAttrVal( toggling_ctl, attr_name_t, c_init );
+	addAttrVal( hider_ctl, attr_name_h, c_init );
 	
 	addListener(
 		toggling_ctl
 		, 'click'
 		, function () {
-			toggleAttrVal( toggling_ctl, attr_name_t, c_less, c_more );
-			toggleAttrVal( hider_ctl, attr_name_h, c_less, c_more );
+			toggleAttrVal( toggling_ctl, attr_name_t, c_init, c_click );
+			toggleAttrVal( hider_ctl, attr_name_h, c_init, c_click );
 		} );
 }
 
-function initToggleAbility ( toggler_attr_name, hider_attr_name, c_less, c_more ) {
+function initToggleAbility ( toggler_attr_name, hider_attr_name, c_init, c_click ) {
 	var togglers = document.querySelectorAll( '[' + toggler_attr_name + ']' );
 	
 	for ( var i = 0, i_ln = togglers.length; i < i_ln; i++ ) {
-		initTogglingCtl( togglers[i], toggler_attr_name, hider_attr_name, c_less, c_more );
+		initTogglingCtl( togglers[i], toggler_attr_name, hider_attr_name, c_init, c_click );
 	}
 }
